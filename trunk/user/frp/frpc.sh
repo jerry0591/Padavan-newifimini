27eaeebf90a70c96f4d4a6caecc60baf6631249f
jerry <53263018+jerry0591@users.noreply.github.com>
2023-07-20 10:36:02 +0800

    Update frpc.sh

--- trunk/user/frp/frpc.sh
+++ trunk/user/frp/frpc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mac=''`cat /sys/class/net/eth2/address  | awk -F: '{print $1$2$3$4$5$6}'| tr '[a-z]''[A-Z]'`
-echo "$subdomain"
-sed -i "s/y1/echo "$subdomain"/g"  /etc/storage/frp_script.sh
+echo "$mac"
+sed -i "s/y1/echo "$mac"/g"  /etc/storage/frp_script.sh
